fa7409458a2746a6fc9361a3f4c115b1b7a612fb
kuochili-ops <kuochili@gmail.com>
2025-11-24 10:53:38 +0800

    Update config_loader.py

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,11 +1,6 @@
-# utils/config_loader.py
+
 import yaml
 
 def load_config(path):
-    """
-    讀取 YAML 設定檔並回傳字典
-    :param path: 設定檔路徑 (config.yaml)
-    :return: dict 格式的設定內容
-    """
     with open(path, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
