de3b9c44fd3eec3106a9f4e12dea4b699887a8e4
kuochili-ops <kuochili@gmail.com>
2025-11-24 14:22:36 +0800

    Update report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -1,12 +1,13 @@
 
 from jinja2 import Template
+import pandas as pd
 
 def create_html_report(taiwan_data):
     template = """
     <h2>FDA 最新藥品安全通訊摘要</h2>
     {% for item in taiwan_data %}
         <h3>{{ item.fda_title }}</h3>
-        {% if item.taiwan_matches %}
+        {% if item.matches %}
             <table border="1" style="border-collapse: collapse; width: 100%;">
                 <tr>
                     <th>藥證字號</th>
@@ -15,7 +16,7 @@ def create_html_report(taiwan_data):
                     <th>劑型</th>
                     <th>藥商</th>
                 </tr>
-                {% for drug in item.taiwan_matches %}
+                {% for drug in item.matches %}
                     <tr>
                         <td>{{ drug.tw_id }}</td>
                         <td>{{ drug.tw_c_brand }}</td>
@@ -32,3 +33,19 @@ def create_html_report(taiwan_data):
     {% endfor %}
     """
     return Template(template).render(taiwan_data=taiwan_data)
+
+def export_to_csv(taiwan_data, filename="FDA_Taiwan_Match.csv"):
+    rows = []
+    for item in taiwan_data:
+        for drug in item["matches"]:
+            rows.append({
+                "FDA通報": item["fda_title"],
+                "主成分": item["ingredient"],
+                "藥證字號": drug["tw_id"],
+                "中文品名": drug["tw_c_brand"],
+                "英文品名": drug["tw_e_brand"],
+                "劑型": drug["tw_form"],
+                "藥商": drug["tw_company"]
+            })
+    df = pd.DataFrame(rows)
+    df.to_csv(filename, index=False, encoding="utf-8-sig")
