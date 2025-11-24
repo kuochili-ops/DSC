3d6f7a82aebab333e2bd47744a89586a70add932
kuochili-ops <kuochili@gmail.com>
2025-11-24 13:19:28 +0800

    Update report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -1,30 +1,27 @@
 
 from jinja2 import Template
-from collections import defaultdict
 
 def create_html_report(taiwan_data):
-    atc_groups = defaultdict(list)
-    for item in taiwan_data:
-        for drug in item["taiwan_matches"]:
-            atc_groups[drug["ATC_class"]].append(drug)
-
     template = """
     <h2>FDA 最新藥品安全通訊摘要</h2>
-    {% for atc_class, drugs in atc_groups.items() %}
-        <h3>{{ atc_class }}</h3>
-        <table border="1">
-            <tr><th>藥證字號</th><th>中文品名</th><th>英文品名</th><th>劑型</th><th>藥商</th><th>ATC code</th></tr>
-            {% for drug in drugs %}
-                <tr>
-                    <td>{{ drug.tw_id }}</td>
-                    <td>{{ drug.tw_c_brand }}</td>
-                    <td>{{ drug.tw_e_brand }}</td>
-                    <td>{{ drug.tw_form }}</td>
-                    <td>{{ drug.tw_company }}</td>
-                    <td>{{ drug.ATC_code }}</td>
-                </tr>
-            {% endfor %}
-        </table>
+    {% for item in taiwan_data %}
+        <h3>{{ item.fda_title }}</h3>
+        {% if item.taiwan_matches %}
+            <table border="1">
+                <tr><th>藥證字號</th><th>中文品名</th><th>英文品名</th><th>劑型</th><th>藥商</th></tr>
+                {% for drug in item.taiwan_matches %}
+                    <tr>
+                        <td>{{ drug.tw_id }}</td>
+                        <td>{{ drug.tw_c_brand }}</td>
+                        <td>{{ drug.tw_e_brand }}</td>
+                        <td>{{ drug.tw_form }}</td>
+                        <td>{{ drug.tw_company }}</td>
+                    </tr>
+                {% endfor %}
+            </table>
+        {% else %}
+            <p>未找到對應的台灣藥品</p>
+        {% endif %}
     {% endfor %}
     """
-    return Template(template).render(atc_groups=atc_groups)
+    return Template(template).render(taiwan_data=taiwan_data)
