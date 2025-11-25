af93a6af999a5e07dd2489ebcbe87d9c062f346e
kuochili-ops <kuochili@gmail.com>
2025-11-25 14:54:37 +0800

    Update fda_scraper.py

--- fda_scraper.py
+++ fda_scraper.py
@@ -15,30 +15,28 @@ def fetch_fda_announcements():
     soup = BeautifulSoup(res.text, "html.parser")
     results = []
 
-    # 公告在 div.views-row 裡
-    for row in soup.select("div.views-row"):
-        title_tag = row.select_one("div.views-field-title a")
-        date_tag = row.select_one("span.date-display-single")
-
-        if not title_tag:
+    # 找到所有 <li> 公告項目
+    for li in soup.select("ul > li"):
+        link = li.find("a")
+        if not link:
             continue
 
-        title = title_tag.get_text(strip=True)
-        href = title_tag.get("href")
+        title = link.get("title") or link.get_text(strip=True)
+        href = link.get("href")
         if not href:
             continue
         if not href.startswith("http"):
             href = "https://www.fda.gov" + href
 
-        # 日期處理：mm/dd/yyyy → dd-mm-yyyy
-        if date_tag:
-            raw_date = date_tag.get_text(strip=True)
-            try:
-                date_fmt = datetime.strptime(raw_date, "%m/%d/%Y").strftime("%d-%m-%Y")
-            except Exception:
-                date_fmt = ""
-        else:
-            date_fmt = ""
+        # 抓 <li> 前段的日期字串
+        text = li.get_text(" ", strip=True)
+        date_str = text[:10]
+
+        # 嘗試轉換成 dd-mm-yyyy
+        try:
+            date_fmt = datetime.strptime(date_str, "%m-%d-%Y").strftime("%d-%m-%Y")
+        except Exception:
+            date_fmt = date_str  # 如果已經是 dd-mm-yyyy 就保留
 
         results.append({
             "date": date_fmt,
